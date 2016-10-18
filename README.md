c6482fbec4a53c203d4e74d4bb211c467f5e0a3a
jhaubold <jhaubold@trek10.com>
2016-10-18 16:02:08 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,95 @@
 # lambda-error-hound
 Utility for quickly finding cloudwatch logs surrounding an lambda error 
+
+## Installation
+
+`npm install -g lambda-error-hound`
+
+## Example
+
+`lambdaErrorHound error -f myfunctionwitherrors`
+
+## Usage
+
+Lambda error hounds has three modes, error, group and list
+
+### List
+This is a convenience operation. It lists either Lambda functions or Cloudwatch log groups.
+
+`lambdaErrorHound list -f` lists all Lambda functions.
+
+`lambdaErrorHound list -g` lists all CloudWatch log groups
+
+
+### Error
+Finds CloudWatch error metrics for the specified function in the specified time interval. Then it pulls logs from the corresponding log group's log streams.
+
+Options
+
+	-r, --region        aws region to use
+
+This is required if the AWS_DEFAULT_REGION environment variable is not set.
+
+	-f, --functionName  Lambda function name                            [required]
+
+Lambda function
+
+	-s, --startDate     search starting date/time           [default: (yesterday)]
+
+Beginning date in format yyyy-mm-dd hh:mm:ss in local time.
+
+	-e, --endDate       search upto date/time                     [default: (now)]
+
+Ending date in format yyyy-mm-dd hh:mm:ss in local time.
+
+	-i, --inteval       search previous n minutes
+
+Sets start time to n minutes in the past
+
+	-a, --after         minutes of logs after error to find           [default: 1]
+
+For every error found get all logs from this many minutes after the time of the error metric.
+This should be greater then the run time of the function.
+
+	-b, --before        minutes of logs before error to find          [default: 1]
+
+For every error found get all logs from this many minutes after the time of the error metric.
+
+	-k, --skip          number of errors to skip                      [default: 0]
+
+Skip this many errors. Useful if you have hundreds of errors and only want to look at a few at a time.
+
+	-t, --take          number of errors to find logs for             [default: 5]
+
+Look at this many errors. Useful if you have hundreds of errors and only want to look at a few at a time.
+
+### Group
+Get Cloudwatch logs from a log group for a specific timeframe.
+
+Options
+
+	-r, --region        aws region to use
+
+This is required if the AWS_DEFAULT_REGION environment variable is not set.
+
+	-g, --group         log group to search                             [required]
+
+Aws Cloudwatch Log group to get logs from. Don't forget leading `/` if it exists
+
+	-s, --startDate     search starting date/time           [default: (yesterday)]
+
+Beginning date in format yyyy-mm-dd hh:mm:ss in local time.
+
+    -a, --after      minutes of logs to get                          [default: 10]
+
+Number of minutes of logs to get.
+
+
+## Credentials
+
+Lambda Error Hound uses the aws nodejs sdk with out specifying credentials.
+This means that the sdk will look for credentials in the following order.
+1. Environment variables AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and optionally AWS_SESSION_TOKEN
+2. If AWS_PROFILE is set that profile is used from the ~/.aws/config file.
+3. EC2 Instance metadata
+
---
+++ index.js
@@ -0,0 +1,137 @@
+#!/usr/bin/env node 
+'use strict'
+
+var Promise     = require('bluebird')
+var options     = require('./lib/options.js')
+var list        = require('./lib/list.js')
+var errorWindow = require('./lib/errorWindow.js')
+
+var aws     = require('aws-sdk')
+var cwl     = Promise.promisifyAll(new aws.CloudWatchLogs({region: options.argv.region }))
+
+if (options.argv._[0] === 'list')
+  if (options.argv.f)
+    return list.listLambdas(options.argv.region)
+  else 
+    return list.listGroups(options.argv.region, options.argv.g === true ? undefined : options.argv.g)
+
+
+var functionName  = options.argv.functionName
+var startDate     = options.argv.startDate
+var endDate       = options.argv.endDate
+var windowBack    = options.argv.before * 60 * 1000
+var windowForward = options.argv.after * 60 * 1000
+var take          = options.argv.take
+var skip          = options.argv.skip
+var logGroupName  = functionName ? '/aws/lambda/' + functionName : options.argv.group
+if (options.argv.interval) {
+  startDate = new Date(+new Date() - (+options.argv.interval * 1000 * 60))
+}
+
+function getStreams (min, max, accumulator, nextToken) {
+  return cwl.describeLogStreamsAsync({
+    logGroupName: logGroupName,
+    descending: true,
+    orderBy: 'LastEventTime',
+    nextToken: nextToken
+  }).then(function (results) {
+    var filtered = results.logStreams.filter(function (ls) {
+      return (ls.lastEventTimestamp >= min && ls.firstEventTimestamp <= max)
+    })
+    accumulator = (accumulator || []).concat(filtered)
+    console.error('Read %d streams, %d are candidates', results.logStreams.length, filtered.length)
+    // we need a better base case,
+    // and results.logStreams[*].firstEventTime
+    if (results.nextToken && (filtered.length > 0 || (accumulator || []).length === 0)) {
+      // recurse
+      return getStreams (min, max, accumulator, results.nextToken)
+    } else {
+      return Promise.resolve(accumulator)
+    }
+  })
+}
+
+var windowList
+
+if (functionName) {
+
+  console.error('Getting logs for function %s', functionName)
+  console.error('Parameters')
+  console.error('----------')
+  console.error('Start Date:', new Date(startDate))
+  console.error('End Date:  ', new Date(endDate))
+  console.error('Log window size: %d minutes before to %d minutes after', options.argv.before, options.argv.after)
+  console.error('Skipping %d errors and taking the next %d', skip, take)
+  console.error()
+
+  windowList = errorWindow.get(options.argv.region, startDate, endDate, functionName, windowForward, windowBack, take, skip)
+
+} else {
+  windowList = Promise.resolve([{
+    min: new Date(startDate).getTime(),
+    max: new Date(startDate).getTime() + windowForward
+  }])
+}
+
+// add streams to window list
+windowList.then(function (windowList) {
+  var max = windowList[0].max
+  var min = windowList[windowList.length -1].min
+  
+  return getStreams(min, max)
+  .then(function (streams) {
+    // add appropriate streams to error windows
+    windowList.forEach(function (errorWindow) {
+      errorWindow.streams = streams.filter(function (s) { 
+        return s.lastEventTimestamp >= errorWindow.min && s.firstEventTimestamp <= errorWindow.max
+      }).map(function (s) { return s.logStreamName })
+    })
+    return windowList
+  })
+})
+.each(function (errorWindow, index, length) {
+  // get the events from the streams
+  errorWindow.events = {}
+  var streamsRead = 0
+  return Promise.map(errorWindow.streams, function (stream, streamIndex, totalStreams) {
+    
+    return Promise.join(cwl.getLogEventsAsync({
+      logGroupName: logGroupName,
+      logStreamName: stream,
+      endTime: errorWindow.max,
+      startTime: errorWindow.min,
+      startFromHead: true
+    }), Promise.delay(1000)) // Promise.delay(1000) getLogEvents api calls are limited to 10 per second
+    .spread(function (results){
+      process.stderr.write('Error ' + (index + 1) + ' of ' + length + ' stream ' + (++streamsRead) + ' of ' + totalStreams + '   \r')
+
+      
+      errorWindow.events[stream] = results.events
+      return errorWindow
+    })
+  }, { concurrency: 10} )
+})
+.each(function (errorWindow) {
+  // now we have a list of errorWindow objects each has list of streams and events object with key=steamname value=list of events
+  console.log()
+  console.log('For logs/errors between %s and %s', 
+              new Date(errorWindow.min).toUTCString(),
+              new Date(errorWindow.max).toUTCString())
+  console.log('==================================================================================')
+  for(var stream in errorWindow.events) {
+    console.log()
+    console.log('Stream Name: %s', stream)
+    console.log('-----------------------------------------------------------------')
+    errorWindow.events[stream].forEach(function (e) {
+      process.stdout.write(e.message)
+    })
+  }
+})
+.catch(function (err) {
+  console.log(err)
+  if (err.Message) {
+    console.error(err.Message)
+  } else {
+    console.error(err)
+  }
+})
